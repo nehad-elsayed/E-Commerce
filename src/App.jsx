d66fdc1e7deff309bb63e4fe33afca6797aaee27
nehad-elsayed <nehad.boody74@gmail.com>
2025-07-02 16:31:59 +0300

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+
+
+
+const router = createBrowserRouter([
+ {path: "/",
+  element: <Layout/>,
+  children: [
+    {path: "/", element: <Home/>},
+    {path: "/products", element: <Products/>},
+    {path: "/login", element: <Login/>},
+    {path: "/register", element: <Register/>},
+   {path: "*", element: <NotFound/>},
+  ]}
+])
 
 
 function App() {
@@ -5,7 +21,7 @@ function App() {
 
   return (
   <>
-  hello world
+ <RouterProvider router={router}></RouterProvider>
   </>
   )
 }
